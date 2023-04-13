9622440a6f197f53bf4084f32eac0dda6ba867bb
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2023-04-13 10:46:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.c6eliqrdd2zx.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chewzian-employee"
+customregion = "us-east-1"
